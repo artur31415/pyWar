8ed9f01faf9ec0ebd76c040e9d87a2444fbe4ebb
Artur Ferreira Moreira <artur31415926@gmail.com>
2022-03-24 16:55:16 -0300

    TANK CODE 2

--- tank.py
+++ tank.py
@@ -3,11 +3,13 @@ import pygame
 
 class Tank:
 
-    def __init__(self, position, name) -> None:
+    def __init__(self, position, name, territory_name, count) -> None:
         self.position = position
         self.name = name
+        self.territory_name = territory_name
+        self.count = count
 
     def draw(self, DISPLAY):
         w = 10
         h = 10
-        pygame.draw.rect(DISPLAY, (255, 255, 255), (self.position[0] + w / 2, self.position[1] + h / 2, w, h))
+        pygame.draw.rect(DISPLAY, (255, 255, 255), (self.position[0] - w / 2, self.position[1] - h / 2, w, h))
